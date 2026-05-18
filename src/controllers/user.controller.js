8d9141b5a94c4835435e13a5b532271f9402e984
Ganesh Rajput <ganeshrajput0448@gmail.com>
2026-05-18 13:07:29 +0530

    alll set up configure

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const registerUser = asyncHandler( async ( req, res) => {
+    res.status(400).json({
+        message: "Ganesh Solanki chai or code"
+    })
+})
+
+export { registerUser }
